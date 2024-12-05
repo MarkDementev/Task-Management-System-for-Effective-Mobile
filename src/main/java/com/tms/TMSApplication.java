94072bec125d07dda88f1e407d0dda377fa8bd3e
MarkDementev <mark_sur@mail.ru>
2024-12-05 14:26:55 +0300

    start project

---
+++ src/main/java/com/tms/TMSApplication.java
@@ -0,0 +1,11 @@
+package com.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMSApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TMSApplication.class, args);
+	}
+}
