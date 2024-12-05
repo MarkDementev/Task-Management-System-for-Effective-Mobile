ef40287676ef229577116788e7c69afad5231259
MarkDementev <mark_sur@mail.ru>
2024-12-05 14:53:58 +0300

    add User and Admin repo-s

---
+++ src/main/java/com/tms/repository/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.tms.repository;
+
+import com.tms.model.Admin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
